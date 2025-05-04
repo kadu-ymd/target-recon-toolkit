366034606dbc60e4b5913e663584549155c7c387
kadu-ymd <carlosepy@al.insper.edu.br>
2025-05-04 14:18:22 -0400

    feat: wafw00f implementado
    Alem disso, foram feitas outras modificacoes para a ferramenta ser interagivel com o usuario (CLI)

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from tools import *
+import json
+
+
+OPTIONS = {"indent": 4, "sort_keys": True}
 
 def main() -> int:
-    # dnslookup.dnslookup("youtube1com")
+    file_list = []
+
+    print("Available tools for use:")
 
     print("1. DNS Lookup\n"
           "2. Portscan\n"
@@ -9,7 +15,29 @@ def main() -> int:
           "4. WafW00f\n"
     )
 
-    wafwoof.wafwoof("youtube.com")
+    tool = int(input("Enter the number of the tool you want to use: "))
+    print()
+
+    if tool == 1:
+        dns_dict = dnslookup.dnslookup("")
+    elif tool == 2:
+        pass
+    elif tool == 3:
+        pass
+    elif tool == 4:
+        print("Tool: WAFW00F")
+
+        domains = input("Enter the domain(s) you want to inspect (separated by space): ")
+        print()
+
+        domains = domains.strip().split(" ")
+
+        waf_dict = ("WAFW00F", wafwoof.wafwoof(domains))
+        file_list.append(waf_dict)
+
+    for f in file_list:
+        with open(f"{f[0]}.json", "w") as file:
+            json.dump(f[1], file, **OPTIONS)
 
     return 0
 
--- tools/wafwoof.py
+++ tools/wafwoof.py
@@ -1,59 +1,61 @@
 import subprocess
 import re
+from typing import List
 
-def wafwoof(domain: str):
+def wafwoof(domains: List[str]):
+    print("Starting WAFW00F...")
+
     waf_dict = {}
 
-    result = subprocess.run(" ".join(["wafw00f", domain]), shell=True, capture_output=True, text=True)
-    
-    result.check_returncode()
-    
-    lista_result = result.stdout.strip().split("\n")
+    for domain in domains:
+        result = subprocess.run(" ".join(["wafw00f", domain]), shell=True, capture_output=True, text=True)
+        
+        result.check_returncode()
+        
+        lista_result = result.stdout.strip().split("\n")
 
-    aux_idx = 0
+        aux_idx = 0
 
-    for i in lista_result:
-        if i == "":
-            aux_idx = lista_result.index(i) + 4
-            break
+        for i in lista_result:
+            if i == "":
+                aux_idx = lista_result.index(i) + 4
+                break
 
-    aux_lista = lista_result[aux_idx:]
+        aux_lista = lista_result[aux_idx:]
 
-    lista_final = [re.compile(r'\x1b\[[0-9;]*m').sub('', item) for item in aux_lista]
+        lista_final = [re.compile(r'\x1b\[[0-9;]*m').sub('', item) for item in aux_lista]
+        lista_final = [item[3:].strip() for item in lista_final]
 
-    lista_final = [item[3:].strip() for item in lista_final]
+        if len(lista_final) <= 1:
+            print(f"-> Nenhum WAF encontrado para o dominio {domain}")
+        else:
+            for i in lista_final:
+                match1 = re.search(r'is behind (.+?) WAF', i)
 
-    if len(lista_final) <= 1:
-        print(f"-> Nenhum WAF encontrado para o dominio {domain}")
-        return 1
+                if match1 is not None:
+                    match = match1
+                    break
+            
+            if match1 is None:
+                for i in lista_final:
+                    match2 = re.search(r'seems to be behind a WAF', i)
 
-    # match = re.search(r'is behind (.+?) WAF', lista_final[2])
+                    if match2 is not None:
+                        match = match2
 
-    # if match:
-    #     print(f"WAF: {match.group(1)}")
-    # else:
-    #     print("WAF nao encontrado")
+                        break
 
-    # if lista_final[0][8:] not in waf_dict:
-    #     waf_dict[lista_final[0][8:]] = []
+            if match:
+                if not match.group(0).startswith("seems to be behind a WAF"):
+                    print(f"-> WAF encontrado para o dominio \x1b[33m{domain}\x1b[0m: \x1b[94m{match.group(1)}\x1b[0m")
 
-    # aux_idx = 0
+                    if domain not in waf_dict:
+                        waf_dict[domain] = {"WAF": [match.group(1)]}
+                    else:
+                        waf_dict[domain]["WAF"].append(match.group(1))
+                else:
+                    print(f"-> WAF nao encontrado para o dominio {domain}")
+            else:
+                print(f"-> WAF nao encontrado para o dominio {domain}")
 
-    # for item in lista_final:
-    #     if item.startswith("Checking"):
-    #         aux_idx = lista_final.index(item)
-    #         break
-    #     elif item.startswith("Generic"):
-    #         aux_idx = lista_final.index(item)
-    #         break
-    
-    # print(result.stdout)
-
-    # aux_list = lista_final[aux_idx:]
-
-    # print(aux_list)
-
-    # if len(aux_list) > 1:
-
-
-    return 
+    return waf_dict
